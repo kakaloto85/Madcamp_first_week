44b4551b0a251a1d0ce999fa2f018f56a3b141f4
kakaloto85 <57130783+kakaloto85@users.noreply.github.com>
2020-07-10 21:27:36 +0900

    No change

---
+++ app/src/main/java/com/example/myapplication/Image_Adapter.java
@@ -0,0 +1,126 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+
+public class Image_Adapter extends BaseAdapter {
+    private String imgData;
+    private String geoData;
+    private ArrayList<String> thumbsDataList;
+    private ArrayList<String> thumbsIDList;
+    private Context mContext;
+    Image_Adapter(Context c){
+        mContext = c;
+        thumbsDataList = new ArrayList<String>();
+        thumbsIDList = new ArrayList<String>();
+        getThumbInfo(thumbsIDList, thumbsDataList);
+    }
+
+
+    public boolean deleteSelected(int sIndex){
+        return true;
+    }
+
+    public int getCount() {
+        return thumbsIDList.size();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null){
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(95, 95));
+            imageView.setAdjustViewBounds(false);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(2, 2, 2, 2);
+        }else{
+            imageView = (ImageView) convertView;
+        }
+        BitmapFactory.Options bo = new BitmapFactory.Options();
+        bo.inSampleSize = 8;
+        Bitmap bmp = BitmapFactory.decodeFile(thumbsDataList.get(position), bo);
+        Bitmap resized = Bitmap.createScaledBitmap(bmp, 95, 95, true);
+        imageView.setImageBitmap(resized);
+
+        return imageView;
+    }
+
+    private void getThumbInfo(ArrayList thumbsIDs, ArrayList thumbsDatas){
+        String[] proj = {MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.SIZE};
+
+        Cursor imageCursor = mContext.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                proj, null, null, null);
+
+        if (imageCursor != null && imageCursor.moveToFirst()){
+            String title;
+            String thumbsID;
+            String thumbsImageID;
+            String thumbsData;
+            String data;
+            String imgSize;
+
+            int thumbsIDCol = imageCursor.getColumnIndex(MediaStore.Images.Media._ID);
+            int thumbsDataCol = imageCursor.getColumnIndex(MediaStore.Images.Media.DATA);
+            int thumbsImageIDCol = imageCursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME);
+            int thumbsSizeCol = imageCursor.getColumnIndex(MediaStore.Images.Media.SIZE);
+            int num = 0;
+            do {
+                thumbsID = imageCursor.getString(thumbsIDCol);
+                thumbsData = imageCursor.getString(thumbsDataCol);
+                thumbsImageID = imageCursor.getString(thumbsImageIDCol);
+                imgSize = imageCursor.getString(thumbsSizeCol);
+                num++;
+                if (thumbsImageID != null){
+                    thumbsIDs.add(thumbsID);
+                    thumbsDatas.add(thumbsData);
+                }
+            }while (imageCursor.moveToNext());
+        }
+        imageCursor.close();
+        return;
+    }
+
+    private String getImageInfo(String ImageData, String Location, String thumbID){
+        String imageDataPath = null;
+        String[] proj = {MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.SIZE};
+        Cursor imageCursor = mContext.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                proj, "_ID='"+ thumbID +"'", null, null);
+
+        if (imageCursor != null && imageCursor.moveToFirst()){
+            if (imageCursor.getCount() > 0){
+                int imgData = imageCursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                imageDataPath = imageCursor.getString(imgData);
+            }
+        }
+        imageCursor.close();
+        return imageDataPath;
+    }
+}
+
+
+
--- app/src/main/java/com/example/myapplication/tab2.java
+++ app/src/main/java/com/example/myapplication/tab2.java
@@ -1,13 +1,21 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class tab2 extends Fragment {
 
@@ -30,14 +38,43 @@ public class tab2 extends Fragment {
             R.drawable.img3,
             R.drawable.img4
     };
+//권한에 대한 응답이 있을때 작동하는 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    { //권한을 허용 했을 경우
+        if(requestCode == 1){
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("MainActivity","권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+    }
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        { temp += Manifest.permission.READ_EXTERNAL_STORAGE + " "; }
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        { temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " "; }
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(getActivity(), temp.trim().split(" "),1);
+        }else { // 모두 허용 상태
+            Toast.makeText(getContext(), "권한을 모두 허용", Toast.LENGTH_SHORT).show(); } }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tab2, container, false);
+        checkSelfPermission();
+
         mgridView = (GridView) view.findViewById(R.id.grid_view);
-        mgridView.setAdapter(new ImageAdapter(getActivity(), imageArray));
+        mgridView.setAdapter(new Image_Adapter(getActivity()));
         return view;
     }
 }
